d404eb3c3e606fddfb80b05dee3616c6745dc4a4
Cody <ccolburn@uco.edu>
2021-05-31 11:32:17 -0500

    signin/signout

--- public/controller/auth.js
+++ public/controller/auth.js
@@ -1,6 +1,8 @@
 import * as Element from '../viewpage/element.js'
 import * as FirebaseController from './firebase_controller.js'
 
+export let currentUser
+
 export function addEventListeners() {
     Element.formSignin.addEventListener('submit', async e => {
         e.preventDefault();
@@ -8,8 +10,43 @@ export function addEventListeners() {
         const password = e.target.password.value;
         try {
             await FirebaseController.signIn(email, password);
+            Element.modalSigninForm.hide();
         } catch (e) {
             console.log(e);
+        }
+    });
+
+    Element.menuSignout.addEventListener('click', () => {
+        try {
+            FirebaseController.signOut();
+        } catch (e) {
+            console.log(e);
+        }
+    })
+
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            // sign in
+            currentUser = user;
+            let elements = document.getElementsByClassName('modal-menus-pre-auth');
+            for (let i = 0; i < elements.length; i++) {
+                elements[i].style.display = 'none';
+            }
+            elements = document.getElementsByClassName('modal-menus-post-auth');
+            for (let i = 0; i < elements.length; i++) {
+                elements[i].style.display = 'block';
+            }
+        } else {
+            // sign out
+            currentUser = null;
+            let elements = document.getElementsByClassName('modal-menus-pre-auth');
+            for (let i = 0; i < elements.length; i++) {
+                elements[i].style.display = 'block';
+            }
+            elements = document.getElementsByClassName('modal-menus-post-auth');
+            for (let i = 0; i < elements.length; i++) {
+                elements[i].style.display = 'none';
+            }
         }
     });
 }
--- public/viewpage/element.js
+++ public/viewpage/element.js
@@ -1,1 +1,11 @@
+// top menu
+export const menuSignout = document.getElementById('menu-signout');
+
+//form
 export const formSignin = document.getElementById('form-signin');
+
+//main content root
+export const root = document.getElementById('root');
+
+//modal Bootstrap object
+export const modalSigninForm = new bootstrap.Modal(document.getElementById('modal-signin-form'));
